160e61b9a1242132033ddf868d4991a719473175
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-03-16 09:45:06 -0500

    [+] NPM > dependencies: Implements jquery, bootstrap, popperjs, fontawesome
    Referencia de implementacion de FontAwesome en Angular
    https://www.angularjswiki.com/angular/how-to-use-font-awesome-icons-in-angular-applications/

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faAngular } from '@fortawesome/free-brands-svg-icons';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,9 +12,14 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+  constructor() {
+    library .add( faAngular );
+  }
+}
