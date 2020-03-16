977e338c33fd4d253a21bfbb14473b0449d567ba
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-03-16 14:11:56 -0500

    [+] ComponentStyles > styles: Style scope

--- src/app/components/component-styles/component-styles.component.ts
+++ src/app/components/component-styles/component-styles.component.ts
@@ -3,7 +3,26 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-component-styles',
   templateUrl: './component-styles.component.html',
-  styleUrls: ['./component-styles.component.css']
+  styles: [
+    'h3 { color: orange; text-align: center }',
+    'small { font-size: .8rem; }',
+    `
+      p {
+        font-size: 1.2rem;
+      }
+      small {
+        color: green;
+        text-transform: uppercase;
+      }
+      button {
+        background-color: blue;
+        border: none;
+        color: white;
+        font-weight: bold;
+        padding: .5rem 1rem;
+      }
+    `
+  ]
 })
 export class ComponentStylesComponent implements OnInit {
 
