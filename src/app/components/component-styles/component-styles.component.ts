21c138c1e3ef436c56470400faba83ae264b864b
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-03-16 14:45:35 -0500

    [+] ComponentStyles > Special selectors: host-context

--- src/app/components/component-styles/component-styles.component.ts
+++ src/app/components/component-styles/component-styles.component.ts
@@ -21,6 +21,7 @@ import { Component, OnInit } from '@angular/core';
         font-weight: bold;
         padding: .5rem 1rem;
       }
+
       /** selector de pseudo-clase :host 
        * Afecta estilos en el elemento que aloja el componente */
       :host {
@@ -33,6 +34,16 @@ import { Component, OnInit } from '@angular/core';
       }
       :host(.active) button {
         background-color: red;
+      }
+      /** Funciona igual que :host 
+       * Es útil aplicar estilos basados ​​en alguna condición fuera de la vista de un componente.
+       */
+      :host-context(.theme-light) {
+        background-color: #f8f9fa;
+      }
+      :host-context(.theme-light) h3 {
+        color: red;
+        text-decoration: underline;
       }
     `
   ]
