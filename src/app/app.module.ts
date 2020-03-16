23c2f3fc8318d78b366a44334ad4b27c3c0947b5
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-03-16 13:45:35 -0500

    [+] NgStyle > directive: style & ngStyle, Fix FontAwesome implementation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeModule, FaIconLibrary } from '@fortawesome/angular-fontawesome';
 import { faAngular } from '@fortawesome/free-brands-svg-icons';
+import { fas } from '@fortawesome/free-solid-svg-icons';
 
 import { AppComponent } from './app.component';
 import { NgStyleComponent } from './components/ng-style/ng-style.component';
@@ -20,8 +20,9 @@ import { NgStyleComponent } from './components/ng-style/ng-style.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { 
-  constructor() {
-    library .add( faAngular );
+export class AppModule {
+  constructor( public library: FaIconLibrary ) {
+    library .addIconPacks( fas );       // Referencia paquetes de iconos
+    library .addIcons( faAngular );     // Referencia iconos]
   }
 }
--- src/app/components/ng-style/ng-style.component.ts
+++ src/app/components/ng-style/ng-style.component.ts
@@ -7,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NgStyleComponent implements OnInit {
 
+  styleAuthor = { 'font-weight': 'bold', 'font-size': '.6rem' };
+  stylePhrase = { 'font-size': '1.2rem' };
+  fontSizePhrase = 1.2;
+  fontSizeAuthor = .8;
+  fontWeightAuthor = 'bold';
+  increasesSize = 12;
+
   constructor() { }
 
   ngOnInit() {
