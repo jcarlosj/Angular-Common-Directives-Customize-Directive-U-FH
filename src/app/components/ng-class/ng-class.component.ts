80dcf322f0f18458ca4cc8baf2e32ab10e441230
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-03-17 12:52:59 -0500

    [+] NgClass > directive: Using Strings, Arrays & Objects

--- src/app/components/ng-class/ng-class.component.ts
+++ src/app/components/ng-class/ng-class.component.ts
@@ -7,6 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NgClassComponent implements OnInit {
 
+  classAlert: string = "alert-primary";    // String con nombre de una clase CSS disponible
+
+  classProperties: Object = {           // Objeto con clases condicionadas
+    'text-center': false,
+    'border-bottom': true,
+    'border-warning': true,
+    'text-warning': true,
+    'pb-2': true
+  };
+
+  conditionalClasses: Object = {           // Objeto con propiedades que condicionan
+    uppercase: true,
+    background: true,
+    center: true
+  };
+
+  prop: Object = {
+    isAuthorized: false
+  }
+
+
   constructor() { }
 
   ngOnInit() {
